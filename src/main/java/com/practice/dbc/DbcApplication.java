2e9c6460dcd8c0f3934be3f9bb6d8b84b34a5406
chickencoc <solutw@gmail.com>
2024-02-07 17:18:35 +0900

    init commit

---
+++ src/main/java/com/practice/dbc/DbcApplication.java
@@ -0,0 +1,13 @@
+package com.practice.dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbcApplication.class, args);
+	}
+
+}
